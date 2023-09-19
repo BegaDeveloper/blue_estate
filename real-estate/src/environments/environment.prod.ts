622269edfbb846e4003adc7be901185a98302fe2
Ismar Begić <80792560+BegaDeveloper@users.noreply.github.com>
2023-09-19 12:15:50 +0200

    Update environment.prod.ts

--- real-estate/src/environments/environment.prod.ts
+++ real-estate/src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   mapbox: {
     accessToken:
-      'pk.eyJ1IjoiaXNtYXJiZWdhYSIsImEiOiJjbDhjemQ1cmIxNDdmM3Vxa3EwOGRrOWRnIn0.fhD7dunfOmiq8CqzyvFeLg',
+      'APIKEY',
   },
 };
